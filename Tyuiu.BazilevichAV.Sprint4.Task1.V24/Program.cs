045c70c71c89dcb6fff9533d87b4701e40e631ff
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-10-28 19:08:52 +0500

    Task1

--- Tyuiu.BazilevichAV.Sprint4.Task1.V24/Program.cs
+++ Tyuiu.BazilevichAV.Sprint4.Task1.V24/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tyuiu.BazilevichAV.Sprint4.Task0.V4.Lib;
+using Tyuiu.BazilevichAV.Sprint4.Task1.V24.Lib;
 
 DataService ds = new DataService();
 
@@ -12,28 +12,41 @@ Console.WriteLine("* Вариант #4                                          
 Console.WriteLine("* Выполнила: Базилевич Алёна Владимировна | ИБКСб-25-1    *");
 Console.WriteLine("***********************************************************");
 Console.WriteLine("* УСЛОВИЕ:                                                *");
-Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов     *");
-Console.WriteLine("* заполненный статическими значениями в диапазоне от 0 до *");
-Console.WriteLine("* 9 подсчитать произведение четных элементов массива.     *");
-Console.WriteLine("* {5 ,3 ,7 ,1 ,3 ,9 ,8 ,7 ,9 ,4}                          *");
+Console.WriteLine("* Дан одномерный целочисленный массив на 16 элементов     *");
+Console.WriteLine("* заполненный значениями с клавиатуры в диапазоне от 2 до *");
+Console.WriteLine("* 5 подсчитать произведение четных элементов массива.     *");
+Console.WriteLine("* С клавиатуры:2,2,2,5,5,3,4,2,5,2,5,3,4,3,5,2            *");
 Console.WriteLine("***********************************************************");
 Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                        *");
 Console.WriteLine("***********************************************************");
 
-int[] numsArray = { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
+int len;
+Console.WriteLine("Введите количество элементов массива: ");
+len = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Исходный массив: ");
-for (int i = 0; i <= numsArray.Length - 1; i++)
+int[] numsArray = new int[len];
+
+for (int i = 0; i <= len - 1; i++)
 {
-    Console.WriteLine(numsArray[i]);
+    Console.WriteLine("Введите значение " + i + " элемента массива: ");
+    numsArray[i] = Convert.ToInt32(Console.ReadLine());
 }
+
+Console.WriteLine();
+Console.WriteLine("Массив:");
+for (int i = 0;i <= len - 1; i++)
+{
+    Console.WriteLine(numsArray[i] + "\t");
+}
+
+Console.WriteLine();
+Console.WriteLine();
 
 Console.WriteLine("***********************************************************");
 Console.WriteLine("* РЕЗУЛЬТАТ:                                              *");
 Console.WriteLine("***********************************************************");
 Console.WriteLine("Результирующий массив: ");
 
-int res = ds.GetMultEvenArrEl(numsArray);
-Console.WriteLine("Произведение четных элементов массива = " + res);
-
+int res = ds.Calculate(numsArray);
+Console.WriteLine(res);
 Console.ReadKey();

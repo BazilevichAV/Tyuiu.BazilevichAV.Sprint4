9eb63cbec7501ef5b67a7bd5da2b09e8b950cb0c
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-10-28 18:44:22 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.BazilevichAV.Sprint4.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BazilevichAV.Sprint4.Task0.V4.Lib;
+namespace Tyuiu.BazilevichAV.Sprint4.Task0.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultEvenArrEl()
+        {
+            DataService ds = new DataService();
+
+            int[] numsArray = { 5, 3, 7, 1, 3, 9, 8, 7, 9, 4 };
+            int res = ds.GetMultEvenArrEl(numsArray);
+            int wait = 32;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}

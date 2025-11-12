de6e231cd1e6fde4f4187232c0e8128cce95d773
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-11-12 11:07:07 +0500

    Task6

---
+++ Tyuiu.BazilevichAV.Sprint4.Task6.V22.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.BazilevichAV.Sprint4.Task6.V22.Lib
+{
+    public class DataService : ISprint4Task6V22
+    {
+        public string[] Calculate(string[] array)
+        {
+            string[] mas = Array.FindAll(array, weather => weather.Length == 4);
+            return mas;
+        }
+    }
+}

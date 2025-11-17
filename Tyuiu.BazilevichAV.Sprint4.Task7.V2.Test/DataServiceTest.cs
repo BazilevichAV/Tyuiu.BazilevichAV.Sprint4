cdf7c28b90d555665c95a4ddba3fa342da2fdd92
BazilevichAV <Tyuiu.BazilevichAV@outlook.com>
2025-11-17 17:41:44 +0500

    Task7

---
+++ Tyuiu.BazilevichAV.Sprint4.Task7.V2.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using Tyuiu.BazilevichAV.Sprint4.Task7.V2.Lib;
+namespace Tyuiu.BazilevichAV.Sprint4.Task7.V2.Test
+
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            string value = "597643158942";
+            int n = 3;
+            int m = 4;
+            int[,] matrix = new int[n, m];
+            int res = ds.Calculate(n, m, value);
+            int wait = 39;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
--- Tyuiu.BazilevichAV.Sprint4.Task7.V2/Program.cs
+++ Tyuiu.BazilevichAV.Sprint4.Task7.V2/Program.cs
@@ -1,37 +1,46 @@
 ﻿using System;
-using Tyuiu.BazilevichAV.Sprint4.Task6.V22.Lib;
+using System.Reflection;
+using Tyuiu.BazilevichAV.Sprint4.Task7.V2.Lib;
 
 DataService ds = new DataService();
 
 Console.Title = "Спринт #4 | Выполнила: Базилевич А.В. | ИБКСб-25-1";
 Console.WriteLine("***********************************************************");
 Console.WriteLine("* Спринт #4                                               *");
-Console.WriteLine("* Тема: Класс Array                                       *");
-Console.WriteLine("* Задание #6                                              *");
-Console.WriteLine("* Вариант #22                                             *");
+Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту *");
+Console.WriteLine("* Задание #7                                              *");
+Console.WriteLine("* Вариант #2                                              *");
 Console.WriteLine("* Выполнила: Базилевич Алёна Владимировна | ИБКСб-25-1    *");
 Console.WriteLine("***********************************************************");
 Console.WriteLine("* УСЛОВИЕ:                                                *");
-Console.WriteLine("* Дан строковый массив данных [Снег, Дождь, Туман, Град,  *");
-Console.WriteLine("* Ветер, Туча, Засуха], используя класс Array, выведите   *");
-Console.WriteLine("* элементы массива, длина которых равна 4 символам.       *");
+Console.WriteLine("* Дана строка из одноразрядных цифр 597643158942.         *");
+Console.WriteLine("* Преобразуйте ее в матрицу 3 на 4 и подсчитайте сумму    *");
+Console.WriteLine("* нечетных чисел.                                         *");
 Console.WriteLine("***********************************************************");
 Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                        *");
 Console.WriteLine("***********************************************************");
 
-string[] weather = { "Снег", "Дождь", "Туман", "Град", "Ветер", "Туча", "Засуха" };
+int n = 3;
+int m = 4;
+int[,] matrix = new int[n, m];
+string value = "597643158942";
 
-Console.WriteLine("Исходный массив:");
-for (int i = 0; i <= weather.Length - 1; i++)
+Console.WriteLine("Массив:");
+for (int i = 0; i < n; i++)
 {
-    Console.WriteLine(weather[i]);
+    for (int j = 0; j < m; j++)
+    {
+        matrix[i, j] = int.Parse(value[i * m + j].ToString());
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
 }
 
 Console.WriteLine("***********************************************************");
 Console.WriteLine("* РЕЗУЛЬТАТ:                                              *");
 Console.WriteLine("***********************************************************");
 
-string[] result = ds.Calculate(weather);
+int res = ds.Calculate(n, m, value);
 
-Console.WriteLine("Количество элементов длина которых равна 4 символам = " + result.Length);
+Console.WriteLine("Сумма нечетных чисел = " + res);
 Console.ReadKey();
